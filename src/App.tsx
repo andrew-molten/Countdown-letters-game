5240d6c8963abaa9e0124fb9efbd8774931f0f4e
Andrew Molten <andrewmolten23@gmail.com>
2024-10-29 19:56:24 +1300

    initial pseudocode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ function App() {
   return (
     <>
       <h1>Countdown letters</h1>
+
+      {/* 
+        1. Allow the user to choose Consonants/Vowels 
+        2. Program should automatically identify the longest possible English word in each round and score the appropriate number of points for the round.
+        3. POINTS - 1 point per letter or 18 points if they have used all 9
+        4. Allow for 4 rounds to be played
+        5. Keep track of total points scored
+      
+      */}
     </>
   )
 }
