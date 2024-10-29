c88ec4185f627060892660bf080c6a35bb151b7b
Andrew Molten <andrewmolten23@gmail.com>
2024-10-29 20:27:01 +1300

    Establish ChooseLettersButtons UI

--- src/components/ChooseLettersButtons.tsx
+++ src/components/ChooseLettersButtons.tsx
@@ -1,8 +1,11 @@
 function ChooseLettersButtons() {
   return (
     <div>
-      <button className="btn">Consonants</button>
-      <button className="btn">Vowels</button>
+      <h1 className="text-3xl mb-3">Do you want a</h1>
+      <button className="btn">Consonant</button>
+      <p className="inline text-xl">or a</p>
+      <button className="btn">Vowel</button>
+      <p className="inline text-xl">next?</p>
     </div>
   )
 }
