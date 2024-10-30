9e622771cbb6ed7714dcb5bcb415d3f1a818b70e
Andrew Molten <andrewmolten23@gmail.com>
2024-10-31 12:03:16 +1300

    initial ideating of longestWord algorithm

--- src/components/LongestWord.tsx
+++ src/components/LongestWord.tsx
@@ -1,5 +1,5 @@
 import { LettersChosen } from '../models'
-import englishWords from '../data/englishWords.json'
+import englishWords from '../data/englishWords.json' // from https://github.com/dwyl/english-words/tree/master
 
 interface Props {
   lettersChosen: LettersChosen
@@ -8,14 +8,34 @@ interface Props {
 function LongestWord({ lettersChosen }: Props) {
   // const eligibleWords =[]
 
-  // Test for words using all letters first & reduce by 1 letter if none are found
+  // lettersChosen.letters
+  const letters = ['g', 'i', 't', 'e', 'q', 'i', 'd', 'v', 'u']
 
-  for (const [key] of Object.entries(englishWords)) {
-    if (key.includes('aals')) {
-      console.log(key)
+  // How many words start with letter 0?
+  // Can I iterate through each word to see if it includes all these letters
+
+  const gWords = []
+  // for (const letter of letters) {
+
+  // Testing for 3 letters but the performance quickly shifts from 2 seconds to >10 seconds
+  // Current ideas are:
+  // - start running some iterations in the background while user chooses letters
+  const i = 0
+  console.log('start')
+  for (let j = 0; j < letters.length; j++) {
+    for (let k = 0; k < letters.length; k++) {
+      for (const [key] of Object.entries(englishWords)) {
+        if (i === j) continue
+        if (k === j) continue
+        if (k === i) continue
+        if (key.startsWith(`${letters[i]}${letters[j]}`)) {
+          gWords.push(key)
+        }
+      }
     }
   }
-  // console.log(englishWords.filter((word) => word.includes('aard')))
+  console.log(gWords.length)
+
   return (
     <div>
       <p className="text-2xl mt-5">
