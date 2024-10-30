a2b54f66fc1dfd5ef1ccd1b234e3ec06645c7a7d
Andrew Molten <andrewmolten23@gmail.com>
2024-10-30 15:50:00 +1300

    add dictionary

--- src/components/LongestWord.tsx
+++ src/components/LongestWord.tsx
@@ -1,11 +1,21 @@
 import { LettersChosen } from '../models'
+import englishWords from '../data/englishWords.json'
 
 interface Props {
   lettersChosen: LettersChosen
 }
 
 function LongestWord({ lettersChosen }: Props) {
-  console.log(lettersChosen)
+  // const eligibleWords =[]
+
+  // Test for words using all letters first & reduce by 1 letter if none are found
+
+  for (const [key] of Object.entries(englishWords)) {
+    if (key.includes('aals')) {
+      console.log(key)
+    }
+  }
+  // console.log(englishWords.filter((word) => word.includes('aard')))
   return (
     <div>
       <p className="text-2xl mt-5">
