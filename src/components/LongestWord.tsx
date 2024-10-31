68f2421a09b22041d7b42551f6da5125efbb4587
Andrew Molten <andrewmolten23@gmail.com>
2024-10-31 15:29:11 +1300

    reduce data to a manageable set by removing non letters

--- src/components/LongestWord.tsx
+++ src/components/LongestWord.tsx
@@ -1,40 +1,77 @@
 import { LettersChosen } from '../models'
 import englishWords from '../data/englishWords.json' // from https://github.com/dwyl/english-words/tree/master
+import { alphabet } from '../data/letters'
 
 interface Props {
   lettersChosen: LettersChosen
 }
 
 function LongestWord({ lettersChosen }: Props) {
-  // const eligibleWords =[]
+  const eligibleWords = []
 
   // lettersChosen.letters
   const letters = ['g', 'i', 't', 'e', 'q', 'i', 'd', 'v', 'u']
 
-  // How many words start with letter 0?
-  // Can I iterate through each word to see if it includes all these letters
+  const nonLetters = alphabet.filter((letter) => !letters.includes(letter))
 
-  const gWords = []
+  // const gWords = []
   // for (const letter of letters) {
 
-  // Testing for 3 letters but the performance quickly shifts from 2 seconds to >10 seconds
-  // Current ideas are:
-  // - start running some iterations in the background while user chooses letters
-  const i = 0
-  console.log('start')
-  for (let j = 0; j < letters.length; j++) {
-    for (let k = 0; k < letters.length; k++) {
-      for (const [key] of Object.entries(englishWords)) {
-        if (i === j) continue
-        if (k === j) continue
-        if (k === i) continue
-        if (key.startsWith(`${letters[i]}${letters[j]}`)) {
-          gWords.push(key)
-        }
-      }
+  const englishWordsArray = []
+
+  // Remove words longer than 9 letters
+  for (const [key] of Object.entries(englishWords)) {
+    if (key.length <= 9) {
+      englishWordsArray.push(key)
     }
   }
-  console.log(gWords.length)
+  console.log(englishWordsArray.length)
+
+  // Remove words containing letters that are not in lettersChosen
+  const removedNonLetters = englishWordsArray.filter((word) => {
+    for (const letter of nonLetters) {
+      if (word.includes(letter)) {
+        return false
+      }
+    }
+    return true
+  })
+
+  console.log(removedNonLetters.length)
+
+  // POSSIBLE NEXT STEPS
+  // - Iterate over the words from longest possible , to check if they exist
+  // - Or remove words containing more of some letters than lettersChosen has
+
+  // for (const word of removedNonLetters) {
+  // }
+
+  // for (let i = 0; i < letters.length; i++) {
+  //   for (let j = 0; j < letters.length; j++) {
+  //     for (let k = 0; k < letters.length; k++) {
+  //       for (let l = 0; l < letters.length; l++) {
+  //         for (const word of removedNonLetters) {
+  //           if (i === j) continue
+  //           if (k === j) continue
+  //           if (k === i) continue
+  //           if (l === j) continue
+  //           if (l === i) continue
+  //           if (l === k) continue
+
+  //           if (
+  //             word.startsWith(
+  //               `${letters[i]}${letters[j]}${letters[k]}${letters[l]}`
+  //             )
+  //           ) {
+  //             eligibleWords.push(word)
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log(eligibleWords.length)
+  // console.log(eligibleWords)
 
   return (
     <div>
--- src/data/letters.ts
+++ src/data/letters.ts
@@ -22,3 +22,32 @@ export const constonants = [
   'z',
 ]
 export const vowels = ['a', 'e', 'i', 'o', 'u']
+
+export const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+]
